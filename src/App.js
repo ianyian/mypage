8bd9932aa4ff376d88961d066a659581e0f67b55
XJ <ianyian@gmail.com>
2021-09-13 01:59:51 +0800

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import myProfileImage from "./img/solvay-conference-5.jpg";
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world !</h1>
+      <h2>this is XJ 123 !</h2>
+      <img src={myProfileImage} alt="profile-image" />
+    </div>
+  );
+}
+
+export default App;
